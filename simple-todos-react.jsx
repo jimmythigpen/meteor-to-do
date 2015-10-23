c8e957da2558cc4369532049961f5d0164814409
Jimmy Thigpen <jmthigpen@me.com>
2015-10-23 08:56:17 -0400

    initial commit, added facebook login

---
+++ simple-todos-react.jsx
@@ -0,0 +1,14 @@
+// Define a collection to hold our tasks
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code is executed on the client only
+  Accounts.ui.config({
+   passwordSignupFields: "USERNAME_ONLY"
+ });
+
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    React.render(<App />, document.getElementById("render-target"));
+  });
+}
